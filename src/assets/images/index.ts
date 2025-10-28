2dad4eaa54cc8bf7f9a92b32fabe015e2a3e6f46
Ha Quanq <quangha0120010@gmail.com>
2025-10-28 22:47:01 +0700

    Add static images/icons with index exports

---
+++ src/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { default as ArrowIcon } from "./icon-arrow.svg";
+export { default as LocationIcon } from "./icon-location.svg";
+export { default as BackgroundPatternDesktop } from "./pattern-bg-desktop.png";
+export { default as BackgroundPatternMobile } from "./pattern-bg-mobile.png";
